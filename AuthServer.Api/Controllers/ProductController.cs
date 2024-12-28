9c388088b75e90c583eaf4f3f77ff9079ae3b3e7
nadialperhan <nadialperhan@gmail.com>
2024-12-28 19:07:09 +0300

    Product endpointi eklendi üyeliksiz deneme için

---
+++ AuthServer.Api/Controllers/ProductController.cs
@@ -0,0 +1,46 @@
+﻿using AuthServer.Core.DTOs;
+using AuthServer.Core.Entity;
+using AuthServer.Core.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthServer.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : CustomBaseController
+    {
+        private readonly IServiceGeneric<Product,ProductDto> _serviceGeneric;
+
+        public ProductController(IServiceGeneric<Product, ProductDto> serviceGeneric)
+        {
+            _serviceGeneric = serviceGeneric;
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> GetProduct()
+        {
+            return ActionResultInstance(await _serviceGeneric.GetAllAsync());
+        }
+        [HttpPost]
+        public async Task<IActionResult> SaveProduct(ProductDto productDto)
+        {
+            return ActionResultInstance(await _serviceGeneric.AddAsync(productDto));
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateProduct(ProductDto productDto)
+        {
+            return ActionResultInstance(await _serviceGeneric.Update(productDto,productDto.Id));
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            return ActionResultInstance(await _serviceGeneric.Remove(id));
+        }
+    }
+}
